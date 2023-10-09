4907856b05a489c54d2c1243e307e94757150916
Ibrahim Abdulkarim <aliveibi080@gmail.com>
2023-10-09 12:54:39 +0100

    Initial Commit

---
+++ src/app.ts
@@ -0,0 +1,15 @@
+import { Response, Request } from "express";
+const express = require('express');
+const app = express();
+const path = require('path');
+const port = 3000;
+app.use(express.static('public'));
+
+app.get('/', (req:Request, res:Response) => {
+    res.sendFile(path.resolve(__dirname, '../public', 'index.html'));
+  });
+  
+
+app.listen(port, () => {
+    console.log('Server is listening on port:',port)
+})

100b09e8b055831451a46e09472fb232dbb61ad4
Ibrahim Abdulkarim <aliveibi080@gmail.com>
2023-10-11 15:10:06 +0100

    feat: modified public/views files and public/styles

--- public/scripts/index.js
+++ public/scripts/index.js
@@ -3,6 +3,9 @@ const updateForm = document.getElementById('showUpdateForm');
 const updateTeachers = document.getElementById('showTeachers');
 const remove = document.getElementById("removeTeachers");
 const showContinuous = document.getElementById("cont-ass");
+const showAddSubjects = document.getElementById('add-subject');
+const deleteSubjects = document.getElementById('delete-subject');
+
 
 function getUpdateForm(){
     updateForm.style.display = 'block'
@@ -14,6 +17,8 @@ function hideDetails(e){
     updateTeachers.style.display = 'none'
     remove.style.display = 'none'
     showContinuous.style.display = 'none'
+    showAddSubjects.style.display = 'none'
+    deleteSubjects.style.display = 'none'
 }
 
 
@@ -27,6 +32,14 @@ function removeTeacher(){
 
 function showContinousCa(){
     showContinuous.style.display = 'block'
+}
+
+function handleAddSubject(){
+    showAddSubjects.style.display = 'block'
+}
+
+function handleRemoveSubject(){
+    deleteSubjects.style.display = 'block'
 }
 
 document.addEventListener('submit', hideDetails);

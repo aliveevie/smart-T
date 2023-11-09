04d075705c1efb361ef46832b7086fa42795c6b2
Ibrahim Abdulkarim <aliveibi080@gmail.com>
2023-11-09 12:24:47 +0100

    Update many files

--- public/scripts/modal.js
+++ public/scripts/modal.js
@@ -1,26 +1,10 @@
 // Get references to the modal and registration button
-const modal = document.getElementById("modal-window");
-const registrationButton = document.getElementById("registration-button");
-const verificationForm = document.getElementById("verification-form");
+document.addEventListener('submit', (e) => {
+    e.preventDefault()
+    const modalWindow = document.getElementById('modal-window');
+    modalWindow.style.display = 'block';
+    document.body.style.overflowY = 'hidden';
 
-// Function to display the modal
-function openModal() {
-    modal.style.display = "block";
-}
-
-// Function to close the modal
-function closeModal() {
-    modal.style.display = "none";
-}
-
-// Event listener to open the modal when the registration button is clicked
-registrationButton.addEventListener("click", openModal);
-
-// Event listener to close the modal when the form is submitted
-verificationForm.addEventListener("submit", function (event) {
-    event.preventDefault(); // Prevent the form from submitting (for demonstration)
-    closeModal();
-    // Here, you can add your logic to process the verification code and continue the registration process.
 });
 
-console.log('Hello');
+

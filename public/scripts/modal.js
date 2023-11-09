6a58ed50041119845ee129afde589a4e15f878c7
Ibrahim Abdulkarim <aliveibi080@gmail.com>
2023-11-09 12:34:44 +0100

    Update register.html

--- public/scripts/modal.js
+++ public/scripts/modal.js
@@ -1,10 +1,20 @@
 // Get references to the modal and registration button
+const token = 9807;
+
 document.addEventListener('submit', (e) => {
     e.preventDefault()
     const modalWindow = document.getElementById('modal-window');
     modalWindow.style.display = 'block';
     document.body.style.overflowY = 'hidden';
-
 });
+
+function handleClick(){
+        if(token===9807){
+            window.location.href = './dashboard.html'
+        }else{
+            const invalid = document.getElementById('invalid');
+            invalid.style.display = 'block';
+        }
+}
 
 

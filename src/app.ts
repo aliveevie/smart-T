56a5201b14ffb41aac01aa33cb78c7b16ec03c04
Ibrahim Abdulkarim <aliveibi080@gmail.com>
2023-11-10 10:15:36 +0100

    Update many files

--- src/app.ts
+++ src/app.ts
@@ -3,13 +3,17 @@ const express = require('express');
 const app = express();
 const path = require('path');
 const port = 3000;
+const db = require('./db');
+
 app.use(express.static('public'));
 
 app.get('/', (req:Request, res:Response) => {
     res.sendFile(path.resolve(__dirname, '../public', 'index.html'));
   });
+
+
   
 
 app.listen(port, () => {
-    console.log('Server is listening on port:',port)
-})
+    console.log('Server is listening on port:',port);
+});
---
+++ src/db.ts
@@ -0,0 +1,20 @@
+const { Pool } = require('pg');
+require('dotenv').config();
+
+// Replace the connection string with your actual PostgreSQL connection string
+const connectionString = process.env.CONNECTION1;
+
+const pool = new Pool({
+  connectionString: connectionString,
+});
+
+// Example query
+pool.query('SELECT NOW()', (err:Error, result:any) => {
+  if (err) {
+    return console.error('Error executing query', err.stack);
+  }
+  console.log('Connected to PostgreSQL database. Current timestamp:', result.rows[0].now);
+  pool.end(); // Close the connection pool
+});
+
+module.exports = pool;

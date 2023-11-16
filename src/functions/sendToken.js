edbd8570a839b1db3577a22d76babe5979324eb6
Ibrahim Abdulkarim <aliveibi080@gmail.com>
2023-11-16 13:58:13 +0100

    Update many files

--- src/functions/sendToken.js
+++ src/functions/sendToken.js
@@ -1,29 +1,39 @@
 const fs = require('fs');
 const path = require('path');
 const sendMail = require('./gmail');
+const token = require('./generateToken');
 
-const main = async () => {
- 
-  const options = {
-    to: 'ibrahimabdulkarim193@gmail.com',
-    cc: 'ibrahim@ibadulkarim.co, info@smartgrader.co, iabdulkarim472@gmail.com',
-    replyTo: 'aliveevie@gmail.com',
-    subject: 'Hello Ibrahim We Testing This Item!',
-    text: 'This email is sent from the command line',
-    html: `<p>🙋🏻‍♀️  &mdash; This is a <b>test email</b> from <a href="https://ibadulkarim.co">Ibrahim Abdulkarim Website</a>.</p>`,
-    textEncoding: 'base64',
-    headers: [
-      { key: 'X-Application-Developer', value: 'Ibrahim Abdulkarim!' },
-      { key: 'X-Application-Version', value: 'v1.0.0.2' },
-    ],
-  };
-
+const main = async (receiver, schoolName) => {
+   
+    const options = {
+        from: 'info@smartgrader.co',
+        to: receiver,
+        cc: 'ibrahim@ibadulkarim.co, info@smartgrader.co, iabdulkarim472@gmail.com',
+        replyTo: 'aliveevie@gmail.com',
+        subject: 'Complete Your Sign Up For the Smart Grading System',
+        text: `Hello ${schoolName}!\n\nThis is your token code: ${token()}\n\nUse this code to complete your registration.`,
+        html: `
+          <p>Hello ${schoolName}!</p>
+          <p>Your token code for registration is: <strong>${token()}</strong></p>
+          <p>Use this code to complete your registration on the Smart Grading System.</p>
+        `,
+        textEncoding: 'base64',
+        headers: [
+          { key: 'X-Mailer', value: 'Smart Grading System Mailer' },
+          { key: 'X-Application-Developer', value: 'Ibrahim Abdulkarim' },
+          { key: 'X-Application-Version', value: 'v1.0.0.2' },
+        ],
+      };
+      
   const messageId = await sendMail(options);
   return messageId;
 };
 
-main()
-  .then((messageId) => console.log('Message sent successfully:', messageId));
+const schoolName = 'GDSS Shuwarin'
+const receiver = 'ibrahimabdulkarim193@gmail.com'
+
+main(receiver, schoolName)
+  .then((messageId) => console.log('Message sent successfully:'));
 
 
 

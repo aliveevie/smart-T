8f66d1febacad19f8988c31b7fd6deafd23e2d73
Ibrahim Abdulkarim <aliveibi080@gmail.com>
2023-12-14 12:11:47 +0100

    Update files

--- public/scripts/dashboard.js
+++ public/scripts/dashboard.js
@@ -1,2 +1,17 @@
-const email = schoolRegistration.getEmail();
-console.log(email);
+// dashboard.js
+
+document.addEventListener('DOMContentLoaded', async () => {
+  // Fetch data from the API endpoint
+  try {
+    const response = await fetch('/api/schools/register');
+    if (response.ok) {
+      const data = await response.json();
+      console.log('Data from API:', data);
+      // Now you can use the data as needed in your dashboard logic
+    } else {
+      console.error('Failed to fetch data from API');
+    }
+  } catch (error) {
+    console.error('Error during API fetch:', error);
+  }
+});

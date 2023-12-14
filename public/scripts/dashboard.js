a810ca8c3c53326894e20246e03a9a0b069144bc
Ibrahim Abdulkarim <aliveibi080@gmail.com>
2023-12-14 23:13:41 +0100

    Update all the files

--- public/scripts/dashboard.js
+++ public/scripts/dashboard.js
@@ -3,27 +3,31 @@
   let params = new URLSearchParams(document.location.search);
   let school_id = params.get("school_id");
 
-  document.addEventListener('DOMContentLoaded', () => {
+  document.addEventListener('DOMContentLoaded', async () => {
     // Assuming school_id is defined before this point
     const url = `/api/schools/update?school_id=${school_id}`;
   
-    const response = fetch(url, {
-      method: 'GET',
-      headers: {
-        'Content-Type': 'application/json',
-      },
-    });
+    try {
+      const response = await fetch(url, {
+        method: 'GET',
+        headers: {
+          'Content-Type': 'application/json',
+        },
+      });
   
-    // You might want to handle the response, for example:
-    response.then((res) => {
-      if (res.ok) {
-        console.log('Request was successful');
-      } else {
-        console.error('Request failed');
+      if (!response.ok) {
+        throw new Error(`HTTP error! Status: ${response.status}`);
       }
-    }).catch((error) => {
+  
+      const responseData = await response.json();
+      document.getElementById('school-name').innerText = responseData.school_name;
+      console.log(responseData);
+      // Do something with the responseData, e.g., update the UI
+    } catch (error) {
       console.error('Error:', error);
-    });
+      // Handle the error, e.g., display an error message to the user
+    }
   });
+  
   
 
